cf6d9a1ba02eb149c9c127560919d170ef4aaca7
Martín Cogo <mcogo@godoycruz.gov.ar>
2025-06-30 13:48:22 -0300

    environment var

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-     proxy: {
-      '/api': {
-         target: 'https://10.1.136.87',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "API_URL");
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      proxy: {
+        "/api": {
+          target: env.API_URL,
           changeOrigin: true,
           secure: false,
-          rewrite: path => path.replace(/^\/api/, '/normita/api/v1/chat/from_contenidos')
+          rewrite: (path) =>
+            path.replace(/^\/api/, "/normita/api/v1/chat/from_contenidos"),
+        },
       },
     },
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-}));
+  };
+});
